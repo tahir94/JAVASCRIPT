fff586c1406d9447616aa3ff1178d4da6ba7e2f7
tahir94 <tahirkhan.on@gmail.com>
2018-03-02 16:51:00 +0500

    remove commits

--- drum kit/index.js
+++ drum kit/index.js
@@ -1,21 +1,3 @@
-
-
-// function playSound(e){
-// 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-// 	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-// 	console.log(key)
-
-// 	if(!audio) return; // stop the function from running all together
-
-// 	key.classList.add('playing')
-// 	audio.currentTime = 0; // rewind it to the start
-// 	audio.play();
-// }
-
-// const keys = Array.from(document.querySelectorAll('.key'));
-// console.log(keys)
-// window.addEventListener('keydown',playSound)
-
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
