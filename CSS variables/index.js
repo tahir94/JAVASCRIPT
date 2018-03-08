de8d1b03602901a6c566c65e768b348b5493094b
tahir94 <tahirkhan.on@gmail.com>
2018-03-08 19:26:14 +0500

    working on css variables

---
+++ CSS variables/index.js
@@ -0,0 +1,9 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleUpdate(){
+ console.log(this.value);
+ 
+}
+
+inputs.forEach(input => input.addEventListener('change',handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove',handleUpdate));
